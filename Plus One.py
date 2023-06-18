2b403855f409bfc3fdbdf81d35e9fb52fc434f0f
Kartik Bhat <76040124+kartik-cyber@users.noreply.github.com>
2023-06-18 19:41:20 +0530

    My code

---
+++ Plus One.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def plusOne(self, digits):
+        n = len(digits)
+        carry = 1  
+
+        for i in range(n - 1, -1, -1):
+            digits[i] += carry  
+            carry = digits[i] // 10 
+            digits[i] %= 10  
+
+        if carry:
+            digits.insert(0, carry)
+
+        return digits
